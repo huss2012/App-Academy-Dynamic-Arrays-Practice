6bb48dfa8ff0f492997ecb2cadabe4a38f0864a2
huss2012 <huss24940@gmail.com>
2024-05-09 06:23:32 +0300

    All test specs are passed.

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,19 +1,32 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
+  constructor(defualtSize = 4) {
 
     // Your code here
-
+    this.data = [];
+    this.capacity = defualtSize;
+    this.length = 0;
+    //allocation:
+    for (let i = 0; i < defualtSize; i++){
+      this.data[i] = undefined;
+    }
   }
 
   read(index) {
 
     // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
 
     // Your code here
+    for (let i = this.length - 1; i >= 0; i--) {
+      this.data[i + 1] = this.data[i];
+    }
+
+    this.data[0] = val;
+    this.length++;
   }
 
 }
